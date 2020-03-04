702b14f4a78e26b8da8e1007abb4f3bfb5479450
kai815 <h.kaizuka815@gmail.com>
2020-03-05 07:22:42 +0900

    add phpdoc to method

--- application/core/ClassLoader.php
+++ application/core/ClassLoader.php
@@ -15,16 +15,41 @@ class ClassLoader
 {
     protected $dirs;
 
+    /**
+     * PHPをオートローダクラスに登録する
+     * 
+     * loadClass()メソッドをオートロード時に呼び出されるように指定
+     * 
+     * @access public
+     */
     public function register()
     {
         spl_autoload_register(array($this, 'loadClass'));
     }
 
+    /**
+     * オートロード時に探すディレクトリを追加する
+     * 
+     * modelとcoreのクラスファイルも読み込むようにする
+     * 
+     * @access public
+     * @param string $dir 
+     * 　　　　オートロードの対象となるディレクトリ(フルパスで指定)
+     */
     public function registerDir($dir)
     {
         $this->dirs[] = $dir;
     }
 
+    /**
+     * クラスファイルの読み込みを行う
+     * 
+     * $dirに指定されたディレクトリの中から「クラス名.php」を探して、
+     * あったら読み込む。
+     * @access public
+     * @param string $class
+     *        クラス名
+     */
     public function loadClass($class)
     {
         foreach ($this->dirs as $dir) {
