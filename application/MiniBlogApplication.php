c64399f101ab1550fac1bf51b5c9e41398efc8c0
kai815 <h.kaizuka815@gmail.com>
2020-03-16 17:56:18 +0900

    create-MiniBlogApplicationClass

---
+++ application/MiniBlogApplication.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * 【共通】ミニブログアプリケーションクラス
+ */
+
+class MiniBlogApplication extends Application
+{
+    protected $login_action = array('account', 'signin');
+
+    /**
+     * ルートディレクトリのパスを返す
+     *
+     * @return string
+     */
+    public function getRootDir()
+    {
+        return dirname(__FILE__);
+    }
+
+    /**
+     * ルーティング定義配列を返す
+     *
+     * アクションを実装していくときに適宜追加
+     *
+     * @return array
+     */
+    protected function registerRoutes()
+    {
+        return array();
+    }
+
+    /**
+     * アプリケションの設定を記載
+     *
+     * 本来ならgitにあげないべきかもだが、仮想環境のものなのでコミットしちゃう
+     */
+    protected function configure()
+    {
+        $this->db_manager->connect('master', array(
+            'dsn'      => 'mysql:dbname=mini_blog',
+            'user'     => 'root',
+            'password' => 'root',
+        ));
+    }
+}
