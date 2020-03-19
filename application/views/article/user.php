6d21d97d7aa8e9246f99c758c5995718ec6982d1
kai815 <h.kaizuka815@gmail.com>
2020-03-19 12:51:01 +0900

    create-following-function

--- application/views/article/user.php
+++ application/views/article/user.php
@@ -2,6 +2,18 @@
 
 <h2><?php echo $this->escape($user['user_name']); ?></h2>
 
+<?php if (!is_null($following)) : ?>
+    <?php if ($following) : ?>
+        <p>フォローしています</p>
+    <?php else : ?>
+        <form action="<?php echo $base_url; ?>/follow" method="post">
+            <input type="hidden" name="_token" value="<?php echo $this->escape($_token); ?>" />
+            <input type="hidden" name="following_name" value="<?php echo $this->escape($user['user_name']); ?>" />
+
+            <input type="submit" value="フォローする" />
+        </form>
+    <?php endif; ?>
+<?php endif; ?>
 <div id="articles">
     <?php foreach ($articles as $article) : ?>
         <?php echo $this->render('article/article', array('article' => $article)); ?>
